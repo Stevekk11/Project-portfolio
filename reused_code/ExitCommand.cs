12e54fa2c9a13994f7553cc6ccddc60a5f9a4251
Stevek <vegh@spsejecna.cz>
2026-01-20 17:08:09 +0100

    Add docs

---
+++ reused_code/ExitCommand.cs
@@ -0,0 +1,14 @@
+﻿using System.Net.Sockets;
+
+namespace Cviceni_3_4;
+
+public class ExitCommand : ICommand
+{
+    public void Execute(TcpClient client, StreamReader sr, StreamWriter sw, TcpServer server)
+    {
+        server.LogoutClient(client);
+        sw.WriteLine("You have been logged out.");
+        sw.Flush();
+        client.Close();
+    }
+}
---
+++ reused_code/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Cviceni_3_4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpServer server = new TcpServer(8086);
+        }
+    }
+}
