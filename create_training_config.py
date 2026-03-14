479b518c3da579644e41002c1d54288df4e921f8
Stevek <vegh@spsejecna.cz>
2026-03-14 17:11:04 +0100

    update training config

--- create_training_config.py
+++ create_training_config.py
@@ -85,7 +85,7 @@ def create_simple_yaml(output_path: str = 'dataset.yaml',
         classes: Dictionary of class_id: class_name
     """
     if classes is None:
-        classes = {0: 'autobus_pid', 1: 'tramvaj_pid', 2: 'other_bus', 3: 'other_tram'}
+        classes = {0: 'autobus_pid', 1: 'other_bus', 2: 'other_tram', 3: 'tramvaj_pid'}
 
     yaml_content = {
         'train': train_images,
